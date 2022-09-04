50f38d4b2896db9a0004578358b87415c94a3ccc
Beka Sturua <beqasturua3@gmail.com>
2022-09-04 11:50:55 +0400

    Exploring the Base Types

---
+++ basics.ts
@@ -0,0 +1,20 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+
+let age: number;
+
+age = 12;
+
+let userName: string;
+
+userName = "Beka";
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+
+
